0c593a469b400ee60c901fbeaef77fc4ce97a194
Roger <evolpirate@gmail.com>
2019-08-13 19:46:52 -0400

    creating the configuration for the genesis block for our test to use it and our code

---
+++ config.js
@@ -0,0 +1,9 @@
+// screen case syntax lets us know its using a hard coded value
+const GENESIS_DATA = { 
+    timestamp: 1,
+    lastHash: '------',
+    hash: 'hash-one',
+    data: []
+ };
+
+ module.exports = { GENESIS_DATA } // key and value are both call GENESIS_DATA
