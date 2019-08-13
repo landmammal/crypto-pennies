2e196a30d42a902f208d7711c7d7beef0a50c9d0
Roger <evolpirate@gmail.com>
2019-08-13 19:49:21 -0400

    creating blocks based on test suite specification *green

--- block.js
+++ block.js
@@ -1,18 +1,29 @@
+const { GENESIS_DATA }  = require('./config');
+const cryptoHash = require('./cryptohash')
+
 class Block {
     // when passing parameters as an object order wont matter
     constructor({ timestamp, lastHash, hash, data }){
-        // this refrences the current instances been created
+    //     // this refrences the current instances been created
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
     }
+
+    // factory method methods that return an instance without calling the constructor
+    static genesisBlock(){
+        const genBlock = new this(GENESIS_DATA);
+        return  genBlock
+    }
+
+    // mining new blocks method from last block and new data to add to block
+    static mineBlock({ lastBlock, data }){ 
+        const minedBlock = new this({timestamp: Date.now() ,lastHash: lastBlock.hash, data });
+        minedBlock.hash = cryptoHash(minedBlock.timestamp, minedBlock.lastHash, data)
+        return minedBlock
+    }
 }
 
-const block1 = new Block({ 
-    timestamp: "01/01/2019", 
-    lastHash: 'foo-lasthash', 
-    hash: "foo-hash", 
-    data: 'foo-data' 
-});
-console.log(block1)
+
+module.exports = Block;
