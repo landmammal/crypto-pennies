967f92cd9416b2684d68e0344155ba70c86cf6ad
Roger <evolpirate@gmail.com>
2019-08-20 16:48:23 -0400

    adding test to make sure each block meets difficulty level criteria

--- block.test.js
+++ block.test.js
@@ -10,20 +10,28 @@ describe('Block', () => {
     const hash = 'a-data';
     const lastHash = 'a-lastHash';
     const data = ['blockchain', 'data'];
-    const block = new Block({ timestamp, lastHash, hash, data }); // if the key an the value are the same name we can declare it just by the name
+    const nonce = 1; // number to change for finding difficulty hash
+    const difficulty = 1;
+    const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty }); // if the key an the value are the same name we can declare it just by the name
+    
     // first is the description of what your testing 
     // second is a callback function to run
-    it('has a timestamp, lastHash, hash and data property', () => {
+
+
+
+    it('has a timestamp, lastHash, hash, data, nonce and difficulty property', () => {
         // takes a value to test the value upon (actual value)
-        expect(block.timestamp).toEqual(timestamp) // the time stamp variable declare on line 5 is the expected value of the actual value of expect argument
-        expect(block.lastHash).toEqual(lastHash) 
-        expect(block.hash).toEqual(hash) 
-        expect(block.data).toEqual(data) 
+        expect(block.timestamp).toEqual(timestamp); // the time stamp variable declare on line 5 is the expected value of the actual value of expect argument
+        expect(block.lastHash).toEqual(lastHash); 
+        expect(block.hash).toEqual(hash); 
+        expect(block.data).toEqual(data); 
+        expect(block.nonce).toEqual(nonce); 
+        expect(block.difficulty).toEqual(difficulty); 
     });
 
     // testing the first block of the blockcahin
     describe('genesis()', ()=> {
-        const genesisBlock = Block.genesisBlock(); // static function dont neet to use or change the data on a specific instance of the class
+        const genesisBlock = Block.genesisBlock(); // static function dont need to use or change the data on a specific instance of the class
 
         it('returns a block instance', () => {
             // instanceof returns true or flase value depending if the first part return
@@ -59,7 +67,21 @@ describe('Block', () => {
         });
 
         it("it creates a sha256 `hash` based on the proper inputs", () => {
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, minedBlock.lastHash, data));
+            expect(minedBlock.hash)
+            .toEqual(
+                cryptoHash(
+                    minedBlock.timestamp,
+                    minedBlock.nonce,
+                    minedBlock.difficulty,
+                    lastBlock.hash,
+                    data
+                )
+            );
+        });
+
+        it('sets a `hash` that matches the difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty));
         });
     })
 })
