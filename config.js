a12fcef8020e62b643a6c9f9315d755488397c85
Roger <evolpirate@gmail.com>
2019-08-20 16:49:03 -0400

    adding difficulty and nonce to genesis block

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 // screen case syntax lets us know its using a hard coded value
+
+const INITIAL_DIFFICULTY = 3; // low difficulty for developing purposes
 const GENESIS_DATA = { 
     timestamp: 1,
     lastHash: '------',
     hash: 'hash-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
  };
 
