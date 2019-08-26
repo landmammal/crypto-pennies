e84b3cde0226aa1f0619f8d092ca8aa05004361c
Roger <evolpirate@gmail.com>
2019-08-26 15:16:53 -0400

    adding default mining rate at which we want blocks to be added to the chain

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // screen case syntax lets us know its using a hard coded value
 
+const MINE_RATE = 1000;
 const INITIAL_DIFFICULTY = 3; // low difficulty for developing purposes
 const GENESIS_DATA = { 
     timestamp: 1,
@@ -10,4 +11,4 @@ const GENESIS_DATA = {
     data: []
  };
 
- module.exports = { GENESIS_DATA } // key and value are both call GENESIS_DATA
+ module.exports = { GENESIS_DATA,  MINE_RATE } // key and value are both call GENESIS_DATA
