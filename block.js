248d6bca549a7840df285602b1ada749de96b091
Roger <evolpirate@gmail.com>
2019-09-02 12:30:24 -0400

    returning default difficulty to 1 and adjusting difficulty for slowly mine blocks

--- block.js
+++ block.js
@@ -31,9 +31,9 @@ class Block {
         do {
             nonce++ ; 
             timestamp = Date.now();
-            difficulty = this.adjustDifficulty({ originalBlock: lastBlock, timestamp });
+            difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
             hash = cryptoHash( timestamp, lastHash, data, nonce, difficulty );
-        } while(hash.substring(0, difficulty) !== '0'.repeat(difficulty))
+        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({ timestamp, lastHash, data, nonce, difficulty, hash });
     }
@@ -42,9 +42,9 @@ class Block {
         let { difficulty } = originalBlock;
         
         // default difficulty 
-        if(difficulty <= 0 ) return 1;
+        if(difficulty < 1 ) return 1;
 
-        if ((timestamp - originalBlock.timestamp) < MINE_RATE) return difficulty - 1;
+        if ((timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty - 1;
         
         return difficulty + 1;
     }
