27e64c2b8c00de31826171327d975e3f2f4b10c8
Roger <evolpirate@gmail.com>
2019-09-02 15:44:31 -0400

    modifing test to check for leading 0 in binary not hex

--- block.test.js
+++ block.test.js
@@ -1,3 +1,4 @@
+const hextoToBinary = require('hex-to-binary');
 const Block = require('./block');
 const { GENESIS_DATA, MINE_RATE } = require('./config')
 const cryptoHash = require('./cryptohash')
@@ -80,7 +81,7 @@ describe('Block', () => {
         });
 
         it('sets a `hash` that matches the difficulty criteria', () => {
-            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+            expect(hextoToBinary(minedBlock.hash).substring(0, minedBlock.difficulty))
                 .toEqual('0'.repeat(minedBlock.difficulty));
         });
 
